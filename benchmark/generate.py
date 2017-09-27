9aa9942fb49d4735eb25e78ed6e174b5697fdb15
Max Linke <max_linke@gmx.de>
2017-09-27 16:21:04 +0200

    allow multiple version arguments

--- benchmark/generate.py
+++ benchmark/generate.py
@@ -8,12 +8,14 @@ from .cli import cli
 from .util import ENV, normalize_host
 
 
-def write_bench(top, tmpl, n, gpu, version, name):
+def write_bench(top, tmpl, n, gpu, version, name, host):
     sim = mds.Sim(
         top['{}/'.format(n)],
         categories={'version': version,
                     'gpu': gpu,
-                    'nodes': n})
+                    'nodes': n,
+                    'host': host,
+                    'name': name})
     # copy input files
     mdp = '{}.mdp'.format(name)
     tpr = '{}.tpr'.format(name)
@@ -28,20 +30,18 @@ def write_bench(top, tmpl, n, gpu, version, name):
 @cli.command()
 @click.option('--name', help='name of tpr/mdp file')
 @click.option('--gpu', is_flag=True, help='run on gpu as well')
-@click.option('--version', help='gromacs module to use')
-@click.option('--top_folder', default=None)
+@click.option('--version', help='gromacs module to use', multiple=True)
 @click.option('--host', help='job template name', default=None)
 @click.option('--max_nodes', help='test up to n nodes', type=int)
-def generate(name, gpu, version, top_folder, host, max_nodes):
-    if top_folder is None:
-        top_folder = version
-        if gpu:
-            top_folder += '-gpu'
-
-    top = dtr.Tree(top_folder)
-
+def generate(name, gpu, version,  host, max_nodes):
     host = normalize_host(host)
     tmpl = ENV.get_template(host)
 
-    for n in range(max_nodes):
-        write_bench(top, tmpl, n + 1, gpu, version, name)
+    for v in version:
+        top_folder = v
+        if gpu:
+            top_folder += '-gpu'
+        top = dtr.Tree(top_folder)
+
+        for n in range(max_nodes):
+            write_bench(top, tmpl, n + 1, gpu, v, name, host)
