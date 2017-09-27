1f1d0fcec8bd2af4853c29b9c370cc4c6930d2d7
Max Linke <max_linke@gmx.de>
2017-09-27 16:07:26 +0200

    add util file

---
+++ benchmark/util.py
@@ -0,0 +1,27 @@
+import socket
+from jinja2 import Environment, PackageLoader
+
+ENV = Environment(loader=PackageLoader('benchmark', 'templates'))
+
+
+def get_possible_hosts():
+    return ENV.list_templates()
+
+
+def guess_host():
+    hostname = socket.gethostbyname()
+    known_hosts = get_possible_hosts()
+    for h in known_hosts:
+        if h in hostname:
+            return h
+
+    return None
+
+
+def normalize_host(host):
+    if host is None:
+        host = guess_host()
+        if host is None:
+            raise RuntimeError(
+                "Couldn't guess host. Please provide explicit host")
+    return host
