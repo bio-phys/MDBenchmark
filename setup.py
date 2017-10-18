3328594c96905745912e65f9f1983a3b875a4408
Michael Gecht <michael.gecht@biophys.mpg.de>
2017-10-18 09:57:17 +0200

    Fix `pip install .`
    Without including the `templates` folder, installation via `pip install .` does
    not work correctly.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='benchmark',
-    version='2.0',
+    version='2.0.1',
     description='benchmark gromacs simulations',
     author='Max Linke, Michael Gecht',
     packages=['benchmark'],
+    package_data={'benchmark': ['templates/*']},
     install_requires=[
-        'numpy>=1.8', 'mdsynthesis', 'click', 'jinja2', 'pandas', 'matplotlib'
+        'numpy>=1.8', 'mdsynthesis', 'click', 'jinja2', 'pandas', 'matplotlib',
+        'python-Levenshtein'
     ],
     entry_points={'console_scripts': ['benchmark=benchmark.cli:cli']},
     zip_safe=False)
