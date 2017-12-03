e1f6f3a409aa488b3433a907dcdad722405b7e97
Max Linke <max_linke@gmx.de>
2017-12-03 16:44:25 +0100

    update version handling
    now we have a __version__ tag in the module

--- mdbenchmark/__init__.py
+++ mdbenchmark/__init__.py
@@ -18,3 +18,5 @@
 # You should have received a copy of the GNU General Public License
 # along with MDBenchmark.  If not, see <http://www.gnu.org/licenses/>.
 from . import analyze, generate, submit
+
+__version__ = '3.0.0'
