639ae6a82f53c64b67ed344a3d5fd48fc768aa94
Max Linke <max_linke@gmx.de>
2017-12-11 23:41:46 +0100

    test testing

---
+++ mdbenchmark/tests/test_testing.py
@@ -0,0 +1,32 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# MDBenchmark
+# Copyright (c) 2017 Max Linke & Michael Gecht and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# MDBenchmark is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MDBenchmark is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MDBenchmark.  If not, see <http://www.gnu.org/licenses/>.
+import pytest
+
+from mdbenchmark.testing import data, datafiles
+
+
+def test_datafiles(datafiles):
+    with pytest.raises(RuntimeError):
+        datafiles['foo']
+
+
+def test_data(data):
+    with pytest.raises(RuntimeError):
+        data['foo']
