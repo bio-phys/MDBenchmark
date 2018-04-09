f599fd114282c547b852e2450f859db586aca086
Michael Gecht <gecht.m@gmail.com>
2018-04-09 11:18:01 +0200

    Fix bug in NAMD name check

--- mdbenchmark/mdengines/namd.py
+++ mdbenchmark/mdengines/namd.py
@@ -77,7 +77,7 @@ def check_input_file_exists(name):
     # Check whether the needed files are there.
     for extension in ['namd', 'psf', 'pdb']:
         if name.endswith('.{}'.format(extension)):
-            name = name[:-1 + len(extension)]
+            name = name[:-2 + len(extension)]
 
         fn = '{}.{}'.format(name, extension)
         if not os.path.exists(fn):
