56bcb2567ee4ae5dfd6da1422620f52e3fe67191
Michael Gecht <gecht.m@gmail.com>
2020-08-05 14:20:54 +0200

    test(test_analyze): fix all tests

--- mdbenchmark/tests/test_analyze.py
+++ mdbenchmark/tests/test_analyze.py
@@ -22,10 +22,11 @@ import numpy as np
 import pandas as pd
 
 from mdbenchmark import cli
-from mdbenchmark.utils import parse_bundle, print_dataframe
+from mdbenchmark.utils import map_columns, parse_bundle, print_dataframe
+from mdbenchmark.versions import Version2Categories
 
 
-def test_analyze_gromacs(cli_runner, tmpdir, data):
+def test_analyze_gromacs(cli_runner, tmpdir, capsys, data):
     """Test that the output is OK when all outputs are fine."""
     with tmpdir.as_cwd():
 
@@ -34,26 +35,44 @@ def test_analyze_gromacs(cli_runner, tmpdir, data):
         )
 
         df = pd.read_csv(data["analyze-files-gromacs.csv"])
-        test_output = print_dataframe(df, False) + "\n"
+        df = df.iloc[:, :-1]
+        df = df.replace(np.nan, "?")
+        version = Version2Categories()
+        print_dataframe(
+            df, columns=map_columns(version.category_mapping, version.analyze_printing),
+        )
+
+        out, _ = capsys.readouterr()
+        out = "Setting up...\n" + out
         assert result.exit_code == 0
-        assert result.output == test_output
+        assert result.output == "\n".join(out.split("\n"))
 
 
-def test_analyze_namd(cli_runner, tmpdir, data):
+def test_analyze_namd(cli_runner, tmpdir, capsys, data):
     with tmpdir.as_cwd():
         result = cli_runner.invoke(
             cli, ["analyze", "--directory={}".format(data["analyze-files-namd"])]
         )
-
+        version = Version2Categories()
         bundle = dtr.discover(data["analyze-files-namd"])
-        df = parse_bundle(bundle)
-        test_output = print_dataframe(df, False) + "\n"
+        df = parse_bundle(
+            bundle,
+            columns=version.analyze_categories,
+            sort_values_by=version.analyze_sort,
+        )
+        df = df.iloc[:, :-1]
+        df = df.replace(np.nan, "?")
+        print_dataframe(
+            df, columns=map_columns(version.category_mapping, version.analyze_printing),
+        )
 
+        out, _ = capsys.readouterr()
+        out = "Setting up...\n" + out
         assert result.exit_code == 0
-        assert result.output == test_output
+        assert result.output == "\n".join(out.split("\n"))
 
 
-def test_analyze_with_errors(cli_runner, tmpdir, data):
+def test_analyze_with_errors(cli_runner, tmpdir, capsys, data):
     """Test that we warn the user of errors in the output files. Also test that we
 show a question mark instead of a float in the corresponding cell.
     """
@@ -62,14 +81,23 @@ show a question mark instead of a float in the corresponding cell.
         result = cli_runner.invoke(
             cli, ["analyze", "--directory={}".format(data["analyze-files-w-errors"])]
         )
-
+        version = Version2Categories()
         bundle = dtr.discover(data["analyze-files-w-errors"])
-        df = parse_bundle(bundle)
+        df = parse_bundle(
+            bundle,
+            columns=version.analyze_categories,
+            sort_values_by=version.analyze_sort,
+        )
+        df = df.iloc[:, :-1]
         df = df.replace(np.nan, "?")
-        test_output = print_dataframe(df, False) + "\n"
+        print_dataframe(
+            df, columns=map_columns(version.category_mapping, version.analyze_printing),
+        )
 
+        out, _ = capsys.readouterr()
+        out = "Setting up...\n" + out
         assert result.exit_code == 0
-        assert result.output == test_output
+        assert result.output == "\n".join(out.split("\n"))
 
 
 def test_analyze_console_messages(cli_runner, tmpdir):
@@ -77,6 +105,6 @@ def test_analyze_console_messages(cli_runner, tmpdir):
     with tmpdir.as_cwd():
         # Test error message if the TPR file does not exist
         result = cli_runner.invoke(cli, ["analyze", "--directory=look_here/"])
-        output = "ERROR There is no data for the given path.\n"
+        output = "Setting up...\nERROR There is no data for the given path.\n"
         assert result.exit_code == 1
         assert result.output == output
